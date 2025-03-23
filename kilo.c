f56e28e851ef24aec35d32940f3a976b12fd1ed5
zulijani <zulijanimatteo@gmail.com>
2025-03-23 12:33:31 +0100

    I have now fully inplemented raw mode, but I require some error handling

--- kilo.c
+++ kilo.c
@@ -13,19 +13,27 @@ void enableRawMode(){
     atexit(disableRawMode);
     struct termios raw = orig_termios;
 
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag &= ~(CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 int main(){
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c!= 'q'){
+    while (1){
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
         if(iscntrl(c)){
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         } else {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
+        if(c == 'q') break;
     }
     return 0;
 }
